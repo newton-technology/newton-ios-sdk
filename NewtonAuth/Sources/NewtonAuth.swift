ce6823159f9a4c49c258787d8490d95deeed45b4
kuznetsovmv <mihasiksli82@gmail.com>
2021-05-24 11:20:01 +0300

    rem throws

--- NewtonAuth/Sources/NewtonAuth.swift
+++ NewtonAuth/Sources/NewtonAuth.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-public func authentication(url: String, clientId: String, realm: String, serviceRealm: String) throws -> NewtonAuthentication {
+public func authentication(url: String, clientId: String, realm: String, serviceRealm: String) -> NewtonAuthentication {
     let urlString: String
     if !url.hasPrefix("https") {
         urlString = "https://\(url)"
