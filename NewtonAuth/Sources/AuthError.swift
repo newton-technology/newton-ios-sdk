84dbd537e3290bd433761119780345b99157855f
kuznetsovmv <mihasiksli82@gmail.com.com>
2022-03-14 18:04:37 +0300

    add new password errors and OTP fields

--- NewtonAuth/Sources/AuthError.swift
+++ NewtonAuth/Sources/AuthError.swift
@@ -45,6 +45,28 @@ public enum AuthErrorCode: String, Decodable {
     case serverError = "server_error"
     /// One time password attemtps exceeded
     case attemptsOtpCheckExceeded = "attempts_otp_check_exceeded"
+    /// Password is blacklisted
+    case invalidPasswordBlacklisted = "invalid_password_blacklisted"
+    /// Password has too few digits
+    case invalidPasswordMinDigits = "invalid_password_min_digits"
+    /// Password was recently used
+    case invalidPasswordHistory = "invalid_password_history"
+    /// Password if too short
+    case invalid_password_min_length = "invalid_password_min_length"
+    /// Password has too few lower case chars
+    case invalidPasswordMinLowerCaseChars = "invalid_password_min_lower_case_chars"
+    /// Password is too long
+    case invalidPasswordMaxLength = "invalid_password_max_length"
+    /// Password equals email
+    case invalidPasswordNotEmail = "invalid_password_not_email"
+    /// Password equals username
+    case invalidPasswordNotUsername = "invalid_password_not_username"
+    /// Invalid password regex pattern
+    case invalidPasswordRegexPattern = "invalid_password_regex_pattern"
+    /// Password has too few special symbols
+    case invalidPasswordMinSpecialChars = "invalid_password_min_special_chars"
+    /// Password has too few uppercase symbols
+    case invalidPasswordMinUpperCaseChars = "invalid_password_min_upper_case_chars"
 }
 
 extension AuthErrorCode {
@@ -59,9 +81,15 @@ public struct AuthError: Decodable {
     public let error: AuthErrorCode
     /// Authentication error description
     public let errorDescription: String?
+    /// Authentication otp checks left
+    public let otpChecksLeft: Int?
+    /// Authentication otp sends left
+    public let otpSendsLeft: Int?
     
     enum CodingKeys: String, CodingKey {
         case error = "error"
         case errorDescription = "error_description"
+        case otpChecksLeft = "otp_checks_left"
+        case otpSendsLeft = "otp_sends_left"
     }
 }
